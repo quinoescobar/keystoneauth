2f4f79a20a9bb2b630c6410a4351300cc1110468
José Joaquín Escobar Gómez <alu0100837094@ull.edu.es>
2017-04-10 19:54:32 +0100

    Doing RefreshToken classes

---
+++ keystoneauth1/identity/v3/refresh_token.py
@@ -0,0 +1,37 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from keystoneauth1.identity.v3 import base
+
+__all__ = ('RefreshTokenMethod', 'RefreshToken')
+
+class RefreshTokenMethod(base.AuthMethod):
+    """ """
+    _method_parameters = ['client_id',
+                          'client_secret',
+                          'refresh_token']
+
+
+    def get_auth_data(self, session, auth, headers, **kwargs):
+
+        return
+
+    def get_cache_id_elements(self):
+
+        return
+
+
+
+class RefreshToken(base.AuthConstructor):
+    """ """
+
+    _auth_method_class = RefreshTokenMethod
